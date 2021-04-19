37fb481a77f50b719a472258762d7b34d7c2b8a1
Orlando <vanegas.orlando@gmail.com>
2021-04-19 08:29:49 -0500

    First Commit

---
+++ script.py
@@ -0,0 +1,21 @@
+import math
+import sys
+
+from os import rename
+
+import requests
+
+
+def greet(who_to_greet):
+    greeting = "Hello, {}".format(who_to_greet)
+    test = "Testing"
+    return greeting
+
+
+print(sys.version)
+print(sys.executable)
+
+r = requests.get("http://wwww.intelsat.com")
+print(r.status_code)
+
+print(greet("Orlando"))
